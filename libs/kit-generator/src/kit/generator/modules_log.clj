13bc16368505d0d69e80347624e69e9a39c40fe0
Marcin Bilski <gyamtso@gmail.com>
2026-01-03 21:24:17 +0100

    WIP: hooks

---
+++ libs/kit-generator/src/kit/generator/modules_log.clj
@@ -0,0 +1,44 @@
+(ns kit.generator.modules-log
+  (:require
+   [clojure.java.io :as jio]
+   [kit.generator.io :as io])
+  (:import
+   java.io.File))
+
+(defn- modules-log-path [modules-root]
+  (str modules-root File/separator "install-log.edn"))
+
+(defn read-modules-log [modules-root]
+  (let [log-path (modules-log-path modules-root)]
+    (if (.exists (jio/file log-path))
+      (io/str->edn (slurp log-path))
+      {})))
+
+(defn write-modules-log [modules-root log]
+  (spit (modules-log-path modules-root) log))
+
+(defn module-installed?
+  [ctx module-key]
+  (let [modules-root (get-in ctx [:modules :root])
+        install-log (read-modules-log modules-root)]
+    (= :success (get install-log module-key))))
+
+(defmacro install-once
+  [ctx module-key & body]
+  `(let [modules-root# (get-in ~ctx [:modules :root])
+         install-log# (read-modules-log modules-root#)]
+     (try
+       ~@body
+       (let [updated-log# (assoc install-log# ~module-key :success)]
+         (write-modules-log modules-root# updated-log#)
+         :success)
+       (catch Exception e#
+         (let [updated-log# (assoc install-log# ~module-key :failed)]
+           (write-modules-log modules-root# updated-log#)
+           (throw e#))))))
+
+(defn installed-modules
+  [ctx]
+  (let [modules-root (get-in ctx [:modules :root])
+        install-log (read-modules-log modules-root)]
+    (keys (filter (fn [[_ status]] (= status :success)) install-log))))
